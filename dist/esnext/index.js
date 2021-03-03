1317f0442a67b4f563c31289770990f888086c2a
Raiondesu <a.raiondesu@gmail.com>
2021-03-03 04:06:50 +0300

    Fix a missing dependency

--- dist/esnext/index.js
+++ dist/esnext/index.js
@@ -1,5 +1,5 @@
 export * from "./use-rx.js";
-export * from "./rx-refs.js";
+export * from "./rx-refs/index.js";
 export * from "./hooks/from.js";
 export * from "./hooks/until.js";
 //# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
 export * from './use-rx';
-export * from './rx-refs';
+export * from './rx-refs/index';
 export * from './hooks/from';
 export * from './hooks/until';
