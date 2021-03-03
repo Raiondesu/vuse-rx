449eb17547cd35ccfcb75e54f1e7f0f0f65ec362
Raiondesu <a.raiondesu@gmail.com>
2021-03-03 04:04:17 +0300

    Fix dist cached dependencies

--- dist/es/util.js
+++
@@ -1,9 +0,0 @@
-import ifConst from 'if-const';
-import { Observable } from 'rxjs';
-import { getCurrentInstance, onUnmounted } from 'vue';
-export const createOnDestroy$ = () => new Observable(ctx => ifConst(getCurrentInstance(), inst => {
-    onUnmounted(() => {
-        ctx.next();
-    }, inst);
-}));
-//# sourceMappingURL=util.js.map
