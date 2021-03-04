19db1e022cd3c7a6ca072fd99dea3d2c4820389f
Raiondesu <a.raiondesu@gmail.com>
2021-03-04 11:12:23 +0300

    Feature docs: self referencing reducers example

--- docs/.vitepress/theme/recipes/counter.vue
+++ docs/.vitepress/theme/recipes/counter.vue
@@ -2,6 +2,8 @@
 import { defineComponent, toRef } from 'vue';
 import { useRxState, syncRef } from 'vuse-rx';
 import { setToWindow } from '../set-window';
+import { of } from 'rxjs';
+import { delay } from 'rxjs/operators';
 import Console from '../console.vue';
 
 export default defineComponent({
@@ -11,12 +13,23 @@ export default defineComponent({
   },
   setup(props) {
     const {
-      actions: { increment, setCount },
+      actions,
       state,
       state$
     } = useRxState({ count: 0 })({
       increment: () => state => ({ count: state.count + 1 }),
-      setCount: (count: string) => ({ count: isNaN(Number(count)) ? 0 : Number(count) }),
+
+      setCount: (count: string) => ({
+        count: isNaN(count) ? 0 : +count
+      }),
+
+      setCountAfter(count: string, timeout: number) {
+        return of(this.setCount(count)).pipe(delay(timeout));
+      },
+
+      incrementAfter(timeout: number) {
+        return state => this.setCountAfter(String(this.increment()(state).count), timeout);
+      },
     });
 
     state$.subscribe(state => console.log('counter: ', state.count));
@@ -24,8 +37,7 @@ export default defineComponent({
     return setToWindow({
       syncRef,
       useRxState,
-      increment,
-      setCount,
+      actions,
       state,
 
       // One-way data binding from reactive state (with type convertation)
@@ -37,10 +49,12 @@ export default defineComponent({
 
 <template>
   <main>
-    <button @click="increment">increment {{ state.count }}</button>
+    <button @click="actions.increment">increment to {{ state.count + 1 }}</button>
+    <button v-if="!simple" @click="actions.incrementAfter(1000)">increment to {{ state.count + 1 }} after 1 sec</button>
     <br>
-    <input v-if="!simple" v-model="countRef"/>
-    <button v-if="!simple" @click="setCount(countRef)">set count to {{ countRef }}</button>
+    <input v-model="countRef"/>
+    <button @click="actions.setCount(countRef)">set count to {{ countRef }}</button>
+    <button v-if="!simple" @click="actions.setCountAfter(countRef, 1000)">set count to {{ countRef }} after 1 sec</button>
   </main>
   <console/>
 </template>
