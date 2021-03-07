cb63a41727325a89eccaf390e720ff33590831a7
Raiondesu <Raiondesu@users.noreply.github.com>
2021-03-07 00:55:49 +0000

    Update docs

--- docs/.vitepress/dist/assets/guide_index.md.28241dcc.lean.js
+++
@@ -1,1 +0,0 @@
-import{o as e,c as s,a as t}from"./app.7096f333.js";const a='{"title":"What is vuse-rx?","description":"","frontmatter":{},"headers":[{"level":2,"title":"Why?","slug":"why"}],"relativePath":"guide/index.md","lastUpdated":1615078357613}',o={},i=t('',10);o.render=function(t,a,o,r,n,l){return e(),s("div",null,[i])};export default o;export{a as __pageData};
