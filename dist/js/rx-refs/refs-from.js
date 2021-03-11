0b795feb353f63a183852fa5c11195e4f325c2a2
Raiondesu <Raiondesu@users.noreply.github.com>
2021-03-11 14:48:12 +0000

    Update dist

--- dist/js/rx-refs/refs-from.js
+++ dist/js/rx-refs/refs-from.js
@@ -6,11 +6,16 @@ const vue_1 = require("vue");
 function refsFrom(input, defaultValues) {
     const next = vue_1.ref(defaultValues === null || defaultValues === void 0 ? void 0 : defaultValues.next);
     const error = vue_1.ref(defaultValues === null || defaultValues === void 0 ? void 0 : defaultValues.error);
-    rxjs_1.from(input).subscribe({
-        next: v => next.value = v,
-        error: v => error.value = v,
-    });
-    return { next, error };
+    const value$ = rxjs_1.from(input);
+    return {
+        next,
+        error,
+        value$,
+        subscription: value$.subscribe({
+            next: v => next.value = v,
+            error: v => error.value = v,
+        })
+    };
 }
 exports.refsFrom = refsFrom;
 //# sourceMappingURL=refs-from.js.map
