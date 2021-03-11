0374190a9dec52060221320e020dbd2a97604f56
Raiondesu <Raiondesu@users.noreply.github.com>
2021-03-11 14:54:26 +0000

    Update dist

--- dist/esnext/rx-refs/sync-ref.js
+++ dist/esnext/rx-refs/sync-ref.js
@@ -1,18 +1,13 @@
-import { computed, ref, watch } from 'vue';
+import { ref, watch } from 'vue';
 export function syncRef(ref1, { to, from }, _ref2) {
-    if (to && from) {
-        return computed({
-            get: () => to(ref1.value),
-            set: v => ref1.value = from(v),
-        });
-    }
     const ref2 = ref(_ref2 ?? ref1.value);
     if (to) {
-        watch(ref1, v => ref2.value = to(v));
+        watch(ref1, v => ref2.value = to(v), this);
     }
-    else if (from) {
-        watch(ref2, v => ref1.value = from(v));
+    if (from) {
+        watch(ref2, v => ref1.value = from(v), this);
     }
     return ref2;
 }
+syncRef.with = (options) => syncRef.bind(options);
 //# sourceMappingURL=sync-ref.js.map
--- dist/js/rx-refs/sync-ref.js
+++ dist/js/rx-refs/sync-ref.js
@@ -3,20 +3,15 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.syncRef = void 0;
 const vue_1 = require("vue");
 function syncRef(ref1, { to, from }, _ref2) {
-    if (to && from) {
-        return vue_1.computed({
-            get: () => to(ref1.value),
-            set: v => ref1.value = from(v),
-        });
-    }
     const ref2 = vue_1.ref(_ref2 !== null && _ref2 !== void 0 ? _ref2 : ref1.value);
     if (to) {
-        vue_1.watch(ref1, v => ref2.value = to(v));
+        vue_1.watch(ref1, v => ref2.value = to(v), this);
     }
-    else if (from) {
-        vue_1.watch(ref2, v => ref1.value = from(v));
+    if (from) {
+        vue_1.watch(ref2, v => ref1.value = from(v), this);
     }
     return ref2;
 }
 exports.syncRef = syncRef;
+syncRef.with = (options) => syncRef.bind(options);
 //# sourceMappingURL=sync-ref.js.map
