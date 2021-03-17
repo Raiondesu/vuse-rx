2ce7c3b49a36956a5a1f4e355ce2d0f82a197b5d
Raiondesu <Raiondesu@users.noreply.github.com>
2021-03-17 08:38:17 +0000

    Update docs

--- docs/.vitepress/dist/assets/api_hooks.md.56e9a497.lean.js
+++
@@ -1,1 +0,0 @@
-import{o as n,c as s,a}from"./app.eed7cc6e.js";const t='{"title":"Vue Lifecycle Hooks","description":"","frontmatter":{},"headers":[{"level":2,"title":"fromHook","slug":"fromhook"}],"relativePath":"api/hooks.md","lastUpdated":1615900984025}',o={},p=a('',8);o.render=function(a,t,o,e,c,u){return n(),s("div",null,[p])};export default o;export{t as __pageData};
---
+++ docs/.vitepress/dist/assets/guide_index.md.fd5b9b30.lean.js
@@ -0,0 +1,1 @@
+import{o as e,c as s,a as t}from"./app.eed7cc6e.js";const a='{"title":"What is vuse-rx?","description":"","frontmatter":{},"headers":[{"level":2,"title":"Why?","slug":"why"}],"relativePath":"guide/index.md","lastUpdated":1615970262184}',o={},i=t('',10);o.render=function(t,a,o,r,n,l){return e(),s("div",null,[i])};export default o;export{a as __pageData};
