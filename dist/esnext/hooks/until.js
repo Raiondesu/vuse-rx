6d87aa249050d66ebd151b8f2666a69fb4da106d
Raiondesu <a.raiondesu@gmail.com>
2021-04-09 17:34:06 +0300

    Fix dist after refactoring

--- dist/esnext/hooks/until.js
+++
@@ -1,6 +0,0 @@
-import { takeUntil } from 'rxjs/operators';
-import { onUnmounted } from 'vue';
-import { fromHook } from './from';
-export const pipeUntil = (hook) => takeUntil(fromHook(hook));
-export const untilUnmounted = (obs) => obs.pipe(pipeUntil(onUnmounted));
-//# sourceMappingURL=until.js.map
