26fcfc6fbea5a749e84dd2a97812affa99e64eb4
Adam Jacob <adam@systeminit.com>
2022-01-24 19:24:46 -0800

    bug(): add missing options to watch in fromRefs
    When calling fromRefs, the documentation says we can pass options to the
    underlying watch. Unfortunately, we didn't actually pass the options to
    the underlying watch call. This PR fixes that. :)

--- dist/esnext/rx-refs/from-ref.js
+++ dist/esnext/rx-refs/from-ref.js
@@ -1,8 +1,8 @@
 import { Observable } from 'rxjs';
 import { watch } from 'vue';
 import { untilUnmounted } from '../operators/until';
-export function fromRef(ref) {
-    return untilUnmounted(new Observable(ctx => watch(ref, value => ctx.next(value))));
+export function fromRef(ref, options) {
+    return untilUnmounted(new Observable(ctx => watch(ref, value => ctx.next(value), options)));
 }
 ;
 //# sourceMappingURL=from-ref.js.map
--- dist/js/rx-refs/from-ref.js
+++ dist/js/rx-refs/from-ref.js
@@ -4,8 +4,8 @@ exports.fromRef = void 0;
 const rxjs_1 = require("rxjs");
 const vue_1 = require("vue");
 const until_1 = require("../operators/until");
-function fromRef(ref) {
-    return until_1.untilUnmounted(new rxjs_1.Observable(ctx => vue_1.watch(ref, value => ctx.next(value))));
+function fromRef(ref, options) {
+    return until_1.untilUnmounted(new rxjs_1.Observable(ctx => vue_1.watch(ref, value => ctx.next(value), options)));
 }
 exports.fromRef = fromRef;
 ;
