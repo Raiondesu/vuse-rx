c3370a642ac83318d3643e8f5d3e27363277882c
Alexey Iskhakov <a.raiondesu@gmail.com>
2023-01-21 02:33:16 +0400

    Feature: switch between next & stable docs from nav at any time

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -2,14 +2,17 @@ import { defineConfig } from 'vitepress'
 import { SitemapStream } from 'sitemap';
 import { resolve } from 'node:path';
 import { createWriteStream } from 'node:fs';
-import { name, description } from '../../package.json';
+import { name, description, version } from '../../package.json';
 
 const links: Array<any> = [];
 
 export default defineConfig({
   title: name,
   description: description,
-  head: [['link', { rel: 'icon', type: 'image/svg+xml', href: '/logo-small.svg' }]],
+  head: [
+    ['link', { rel: 'icon', type: 'image/svg+xml', href: '/logo-small.svg' }],
+    ['meta', { name: 'theme-color', content: '#3c8772' }]
+  ],
 
   lastUpdated: true,
 
@@ -27,13 +30,17 @@ export default defineConfig({
 
     nav: [
       {
-        text: 'NPM',
-        link: 'https://npmjs.com/vuse-rx'
+        text: version,
+        items: [
+          { text: 'next', link: 'https://next.vuse-rx.raiondesu.dev' },
+          { text: 'stable', link: 'https://vuse-rx.raiondesu.dev' },
+        ]
       }
     ],
 
     socialLinks: [
       { icon: 'github', link: 'https://github.com/raiondesu/vuse-rx' },
+      { icon: { svg: '<svg viewBox="0 0 27.23 27.23"><rect fill="#333333" width="27.23" height="27.23" rx="2"></rect><polygon fill="#fff" points="5.8 21.75 13.66 21.75 13.67 9.98 17.59 9.98 17.58 21.76 21.51 21.76 21.52 6.06 5.82 6.04 5.8 21.75"></polygon></svg>' }, link: 'https://npmjs.com/vuse-rx' },
     ],
 
     sidebar: [
@@ -65,6 +72,11 @@ export default defineConfig({
     editLink: {
       pattern: 'https://github.com/raiondesu/vuse-rx/edit/dev/docs/:path',
       text: 'Edit this page on GitHub'
+    },
+
+    footer: {
+      message: 'Released under the MIT License.',
+      copyright: 'Copyright © 2021-present Alexey Iskhakov'
     }
   },
 
