74ca4222995888520b6451cb7d82f0eb3517df2a
Alexey Iskhakov <a.raiondesu@gmail.com>
2023-01-22 18:14:41 +0400

    Add jsdoc comments to operators

--- src/operators/set-ref.ts
+++ src/operators/set-ref.ts
@@ -1,4 +1,7 @@
 import { tap } from 'rxjs/operators';
 import { Ref } from 'vue';
 
+/**
+ * Sets a ref's value to current observable value.
+ */
 export const setRef = <T>(ref: Ref<T>) => tap<T>({ next: v => ref.value = v });
